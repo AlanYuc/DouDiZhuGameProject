d4c329f84bff8af03782a0b918f7fa02b2049950
AlanYuc <alan.yuc@outlook.com>
2025-04-19 21:38:53 +0800

    添加服务端连接测试

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    int time = 0;
+    MsgTest msgTest = new MsgTest();
     // Start is called before the first frame update
     void Start()
     {
-        MsgTest msgTest = new MsgTest();
-        msgTest.id = "123";
-        //测试编码
-        byte[] bytes = MsgBase.Encode(msgTest);
-        Debug.Log(Encoding.UTF8.GetString(bytes));
-        //测试解码
-        MsgTest msgDecodeTest = MsgBase.Decode("MsgTest", bytes, 0, bytes.Length) as MsgTest;
-        Debug.Log(msgDecodeTest.id);
-        //测试对协议名字符串的编码和解码
-        byte[] bytesName = MsgBase.EncodeProtocolName(msgTest);
-        int count;
-        string msgProtocolName = MsgBase.DecodeProtocolName(bytesName, 0, out count);
-        Debug.Log(msgProtocolName);
-        Debug.Log(count);
+        ////客户端编码解码测试
+        //MsgTest msgTest = new MsgTest();
+        //msgTest.id = "123";
+        ////测试编码
+        //byte[] bytes = MsgBase.Encode(msgTest);
+        //Debug.Log(Encoding.UTF8.GetString(bytes));
+        ////测试解码
+        //MsgTest msgDecodeTest = MsgBase.Decode("MsgTest", bytes, 0, bytes.Length) as MsgTest;
+        //Debug.Log(msgDecodeTest.id);
+        ////测试对协议名字符串的编码和解码
+        //byte[] bytesName = MsgBase.EncodeProtocolName(msgTest);
+        //int count;
+        //string msgProtocolName = MsgBase.DecodeProtocolName(bytesName, 0, out count);
+        //Debug.Log(msgProtocolName);
+        //Debug.Log(count);
+
+        //服务端测试
+        NetManager.Connect("127.0.0.1", 8888);
+    }
+
+    private void Update()
+    {
+        NetManager.Update();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            NetManager.Send(msgTest);
+        }
     }
 }
 
