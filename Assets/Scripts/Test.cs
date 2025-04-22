61cfa3add9329c965725959716797be366cf86a2
AlanYuc <alan.yuc@outlook.com>
2025-04-22 11:07:47 +0800

    修改测试代码

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    int time = 0;
     MsgTest msgTest = new MsgTest();
     // Start is called before the first frame update
     void Start()
