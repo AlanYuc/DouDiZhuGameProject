81464c9cf43a1f1707f93a3fb139d3998b625b98
AlanYuc <alan.yuc@outlook.com>
2025-04-30 00:19:41 +0800

    随机选一个玩家开始叫地主

--- Assets/Scripts/Panel/GamePanel.cs
+++ Assets/Scripts/Panel/GamePanel.cs
@@ -9,6 +9,23 @@ public class GamePanel : BasePanel
     /// 玩家游戏物体
     /// </summary>
     public GameObject playerObj;
+    /// <summary>
+    /// "叫地主"的按钮
+    /// </summary>
+    public Button callLandlordButton;
+    /// <summary>
+    /// "不叫"的按钮
+    /// </summary>
+    public Button notCallLandlordButton;
+    /// <summary>
+    /// "抢地主"的按钮
+    /// </summary>
+    public Button robLandlordButton;
+    /// <summary>
+    /// "不抢"的按钮
+    /// </summary>
+    public Button notRobLandlordButton;
+
 
     public override void OnInit()
     {
@@ -19,19 +36,34 @@ public class GamePanel : BasePanel
     public override void OnShow(params object[] para)
     {
         //寻找组件
-        playerObj = skin.transform.Find("Player").gameObject;
+        playerObj               = skin.transform.Find("Player").gameObject;
+        callLandlordButton      = skin.transform.Find("CallLandlord_Btn").GetComponent<Button>();
+        notCallLandlordButton   = skin.transform.Find("NotCallLandlord_Btn").GetComponent<Button>();
+        robLandlordButton       = skin.transform.Find("RobLandlord_Btn").GetComponent<Button>();
+        notRobLandlordButton    = skin.transform.Find("NotRobLandlord_Btn").GetComponent<Button>();
+
+        callLandlordButton.gameObject.SetActive(false);
+        notCallLandlordButton.gameObject.SetActive(false);
+        robLandlordButton.gameObject.SetActive(false);
+        notRobLandlordButton.gameObject.SetActive(false);
 
         //添加消息监听
         NetManager.AddMsgListener("MsgGetCardList", OnMsgGetCardList);
+        NetManager.AddMsgListener("MsgGetStartPlayer", OnMsgGetStartPlayer);
 
         //发送请求消息，先获取一副扑克牌
         MsgGetCardList msgGetCardList = new MsgGetCardList();
         NetManager.Send(msgGetCardList);
+
+        //发送请求消息,知道从哪个玩家开始叫地主
+        MsgGetStartPlayer msgGetStartPlayer = new MsgGetStartPlayer();
+        NetManager.Send(msgGetStartPlayer);
     }
 
     public override void OnClose()
     {
         NetManager.RemoveMsgListener("MsgGetCardList", OnMsgGetCardList);
+        NetManager.RemoveMsgListener("MsgGetStartPlayer", OnMsgGetStartPlayer);
     }
 
     public void OnMsgGetCardList(MsgBase msgBase)
@@ -65,6 +97,21 @@ public class GamePanel : BasePanel
             image.rectTransform.sizeDelta = new Vector2(80, 100);
             cardObj.transform.SetParent(playerObj.transform.Find("Cards"), false);
             cardObj.layer = LayerMask.NameToLayer("UI");
+        }
+    }
+
+    /// <summary>
+    /// 当前玩家是否需要叫地主
+    /// </summary>
+    /// <param name="msgBase"></param>
+    public void OnMsgGetStartPlayer(MsgBase msgBase)
+    {
+        MsgGetStartPlayer msgGetStartPlayer = msgBase as MsgGetStartPlayer;
+
+        if(GameManager.playerId == msgGetStartPlayer.playerId)
+        {
+            callLandlordButton.gameObject.SetActive(true);
+            notCallLandlordButton.gameObject.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Protocol/Game/MsgGetStartPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 获取开始玩家的id，表示从哪个玩家开始进行叫地主的操作
+/// </summary>
+public class MsgGetStartPlayer : MsgBase
+{
+    public string playerId = "";
+
+    public MsgGetStartPlayer()
+    {
+        protocolName = "MsgGetStartPlayer";
+    }
+}
